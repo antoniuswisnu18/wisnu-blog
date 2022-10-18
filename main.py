da48964079f296982a21c4b79264a5f2817b00a3
Wisnu <wisnuprasojo14@gmail.com>
2022-10-18 09:19:59 +0700

    Adding virtual env for sensitive key

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ from sqlalchemy.orm import relationship, declarative_base
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, UserRegisterForm, UserLoginForm, CommentForm
 from flask_gravatar import Gravatar
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('app_secret_key')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
